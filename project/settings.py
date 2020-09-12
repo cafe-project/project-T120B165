235d3b5861ea04f9fe8a959c75de49451f8d7047
indre-idenfy <indre@idenfy.com>
2020-09-12 10:51:17 +0300

    requirements

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,3 +148,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
